dba1bcdebf2cf966b2b11b653612520e6f81d8e8
Andrey <andrey_otin@mail.ru>
2022-11-03 23:56:09 +0300

    refactor: перенос функций в модуль комент

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,56 +1,24 @@
 import { toggleClass, isEscapeKey } from './dom-util.js';
-import { createComments, createSocialCommentsElement } from './comment.js';
+import { renderComments, clearComments, resetComments, toggleCommentLoaderHiddenState } from './comment.js';
 
 const bigPictureElement = document.querySelector('.big-picture ');
 const imgElement = bigPictureElement.querySelector('.big-picture__img img');
 const likesElement = bigPictureElement.querySelector('.likes-count');
 const commentsElement = bigPictureElement.querySelector('.comments-count');
-const commentsContainerElement = bigPictureElement.querySelector('.social__comments');
 const closeButtonElement = bigPictureElement.querySelector('#picture-cancel');
 const commentLoaderElement = bigPictureElement.querySelector('.comments-loader');
 
-const NUMBER_OF_COMMENTS = 2;
-
-let current = 0;
-let currentComments;
+let currentComments = {};
 
 const toggleBigPictureHiddenState = () => {
   toggleClass(bigPictureElement, 'hidden');
   toggleClass(document.body, 'modal-open');
-
-  if (commentLoaderElement.classList.contains('hidden')) {
-    toggleClass(commentLoaderElement, 'hidden');
-  }
+  toggleCommentLoaderHiddenState();
 };
 
 const setBigPictureEscdown = () => document.addEventListener('keydown', bigPictureEscKeydownHandler);
 
 const removeBigPictureEscKeydown = () => document.removeEventListener('keydown', bigPictureEscKeydownHandler);
-
-const clearComments = () => {
-  commentsContainerElement.innerHTML = '';
-  commentsContainerElement.previousElementSibling.remove();
-};
-
-const renderComments = (comments) => {
-  current = current + NUMBER_OF_COMMENTS > comments.length
-    ? comments.length
-    : current + NUMBER_OF_COMMENTS;
-
-  if (comments.length === current) {
-    toggleClass(commentLoaderElement, 'hidden');
-  }
-
-  commentsContainerElement.insertAdjacentHTML('beforebegin', createSocialCommentsElement(current, comments.length));
-  commentsContainerElement.insertAdjacentHTML('beforeend', createComments(comments.slice(0, current)));
-};
-
-const setCommentLoaderClick = () => {
-  commentLoaderElement.addEventListener('click', () => {
-    clearComments();
-    renderComments(currentComments);
-  });
-};
 
 const showBigPicture = (data) => {
   currentComments = data.comments;
@@ -69,7 +37,7 @@ function bigPictureEscKeydownHandler(evt) {
   if (isEscapeKey(evt)) {
     removeBigPictureEscKeydown();
     toggleBigPictureHiddenState();
-    current = 0;
+    resetComments();
   }
 }
 
@@ -77,7 +45,14 @@ const setBigPictureClose = () => {
   closeButtonElement.addEventListener('click', () => {
     removeBigPictureEscKeydown();
     toggleBigPictureHiddenState();
-    current = 0;
+    resetComments();
+  });
+};
+
+const setCommentLoaderClick = () => {
+  commentLoaderElement.addEventListener('click', () => {
+    clearComments();
+    renderComments(currentComments);
   });
 };
 
--- js/comment.js
+++ js/comment.js
@@ -1,3 +1,12 @@
+import { toggleClass } from './dom-util.js';
+
+const commentLoaderElement = document.querySelector('.comments-loader');
+const commentsContainerElement = document.querySelector('.social__comments');
+
+const NUMBER_OF_COMMENTS = 2;
+
+let renderedCommentsCount = 0;
+
 const createComment = (avatar, name, message) =>
   `<li class="social__comment">
     <img
@@ -14,4 +23,34 @@ const createComments = (comments) =>
 
 const createSocialCommentsElement = (count, allCount) => `<div class="social__comment-count">${count} из <span class="comments-count">${allCount}</span> комментариев</div>`;
 
-export { createComments, createSocialCommentsElement };
+const resetComments = () => {
+  renderedCommentsCount = 0;
+};
+
+const clearComments = () => {
+  commentsContainerElement.innerHTML = '';
+  commentsContainerElement.previousElementSibling.remove();
+};
+
+const renderComments = (comments) => {
+
+  renderedCommentsCount = renderedCommentsCount + NUMBER_OF_COMMENTS > comments.length
+    ? comments.length
+    : renderedCommentsCount + NUMBER_OF_COMMENTS;
+
+  if (comments.length === renderedCommentsCount) {
+    toggleClass(commentLoaderElement, 'hidden');
+  }
+
+  commentsContainerElement.insertAdjacentHTML('beforebegin', createSocialCommentsElement(renderedCommentsCount, comments.length));
+  commentsContainerElement.insertAdjacentHTML('beforeend', createComments(comments.slice(0, renderedCommentsCount)));
+};
+
+
+const toggleCommentLoaderHiddenState = () => {
+  if (commentLoaderElement.classList.contains('hidden')) {
+    toggleClass(commentLoaderElement, 'hidden');
+  }
+};
+
+export { renderComments, clearComments, resetComments, toggleCommentLoaderHiddenState};
